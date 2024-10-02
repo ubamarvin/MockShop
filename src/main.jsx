da95dfbb7d8ad31599cecc5990602d127caa937a
MarvinUba <ubamarvin95@gmail.com>
2024-10-02 10:48:12 +0200

    wrapped <RouterProvider /> in <StoreDataProvider/> from DataProvider.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
+//main.jsx
+// Classes and Functions
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+//DataProviders
+import { StoreDataProvider } from './Context/DataProvider'
+
+//Routes
 import Root from './Routes/root'
 import Shop from './Routes/shop'
 import Home from './Routes/home'
@@ -26,7 +32,9 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <StoreDataProvider>
+      <RouterProvider router={router} />
+    </StoreDataProvider>
   </StrictMode>,
 )
 
